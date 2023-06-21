df42ea89276d1a2af1c34a180e0db96d40549ee0
silvavake <137318316+silvavake@users.noreply.github.com>
2023-06-21 10:31:49 -0600

    Add files via upload

---
+++ Technology App Final/ViewController.swift
@@ -0,0 +1,205 @@
+//
+//  ViewController.swift
+//  Technology App Final
+//
+//  Created by Vale Silva on 16/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var behavior = true
+    @IBOutlet weak var Mon1: UIButton!
+    @IBOutlet weak var Tues1: UIButton!
+    @IBOutlet weak var Wed1: UIButton!
+    @IBOutlet weak var Thurs1: UIButton!
+    @IBOutlet weak var Fri1: UIButton!
+    @IBOutlet weak var Mon2: UIButton!
+    @IBOutlet weak var Tues2: UIButton!
+    @IBOutlet weak var Wed2: UIButton!
+    @IBOutlet weak var Thurs2: UIButton!
+    @IBOutlet weak var Fri2: UIButton!
+    @IBOutlet weak var Mon3: UIButton!
+    @IBOutlet weak var Tues3: UIButton!
+    @IBOutlet weak var Wed3: UIButton!
+    @IBOutlet weak var Thurs3: UIButton!
+    @IBOutlet weak var Fri3: UIButton!
+    @IBOutlet weak var Mon4: UIButton!
+    @IBOutlet weak var Tues4: UIButton!
+    @IBOutlet weak var Wed4: UIButton!
+    @IBOutlet weak var Thurs4: UIButton!
+    @IBOutlet weak var Fri4: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func Mon1(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Mon1.backgroundColor = .green
+        } else {
+            Mon1.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Tues1(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Tues1.backgroundColor = .green
+        } else {
+            Tues1.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Wed1(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Wed1.backgroundColor = .green
+        } else {
+            Wed1.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Thurs1(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Thurs1.backgroundColor = .green
+        } else {
+            Thurs1.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Fri1(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Fri1.backgroundColor = .green
+        } else {
+            Fri1.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Mon2(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Mon2.backgroundColor = .green
+        } else {
+            Mon2.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Tues2(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Tues2.backgroundColor = .green
+        } else {
+            Tues2.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Wed2(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Wed2.backgroundColor = .green
+        } else {
+            Wed2.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Thurs2(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Thurs2.backgroundColor = .green
+        } else {
+            Thurs2.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Fri2(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Fri2.backgroundColor = .green
+        } else {
+            Fri2.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Mon3(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Mon3.backgroundColor = .green
+        } else {
+            Mon3.backgroundColor = .red
+        }
+    }
+    @IBAction func Tues3(_ sender: UIButton) {
+        behavior.toggle()
+        if behavior {
+            Tues3.backgroundColor = .green
+        } else {
+            Tues3.backgroundColor = .red
+        }
+    }
+    @IBAction func Wed3(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Wed3.backgroundColor = .green
+        } else {
+            Wed3.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Thurs3(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Thurs3.backgroundColor = .green
+        } else {
+            Thurs3.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Fri3(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Fri3.backgroundColor = .green
+        } else {
+            Fri3.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Mon4(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Mon4.backgroundColor = .green
+        } else {
+            Mon4.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Tues4(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Tues4.backgroundColor = .green
+        } else {
+            Tues4.backgroundColor = .red
+        }
+    }
+    @IBAction func Wed4(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Wed4.backgroundColor = .green
+        } else {
+            Wed4.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Thurs4(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Thurs4.backgroundColor = .green
+        } else {
+            Thurs4.backgroundColor = .red
+        }
+    }
+    
+    @IBAction func Fri4(_ sender: UIButton) { behavior.toggle()
+        if behavior {
+            Fri4.backgroundColor = .green
+        } else {
+            Fri4.backgroundColor = .red
+        }
+    }
+}
+
